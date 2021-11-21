d43f5d6023bb1cda399921205fbf16fae146de6f
Eric Zepeda <zepedaeric@gmail.com>
2021-11-21 01:47:47 -0800

    Add required flags

--- nba.py
+++ nba.py
@@ -7,13 +7,13 @@ def print_nba_teams():
 
 parser.add_argument('--team_name', type=str, required=True)
 
-parser.add_argument('--results', type=str,
+parser.add_argument('--results', type=str, required=False,
                     help='get the results for a team')
-parser.add_argument('--prices', type=str,
+parser.add_argument('--prices', type=str, required=False,
                     help='get ticket prices for current team')
-parser.add_argument('--are_playing', type=bool,
+parser.add_argument('--are_playing', type=str, required=False,
                     help='is the team playing today')
-parser.add_argument('--next_game', type=str,
+parser.add_argument('--next_game', type=str, required=False,
                     help='next game')
 
 args = parser.parse_args()
