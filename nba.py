0165921af8681428d2ba076f8d533292581ce120
Eric Zepeda <zepedaeric@gmail.com>
2021-11-21 01:20:06 -0800

    Continue adding options to CLI tool

--- nba.py
+++ nba.py
@@ -4,7 +4,13 @@ parser = argparse.ArgumentParser(description='Get ticket info and NBA results.')
 
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
                     help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
+parser.add_argument('--results', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+parser.add_argument('--prices', action='store_const',
+                    const=sum, default=max,
+                    help='get price for current team')
+parser.add_argument('--are_playing', dest='accumulate', action='store_const',
                     const=sum, default=max,
                     help='sum the integers (default: find the max)')
 
