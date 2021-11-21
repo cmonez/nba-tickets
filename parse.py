f52a356470ee26590ac28fe98582642c3d6c02af
Eric Zepeda <zepedaeric@gmail.com>
2021-11-21 14:28:42 -0800

    continue with game results logic

--- parse.py
+++ parse.py
@@ -20,4 +20,22 @@ def extract_opposing_team_name(node):
   opposing_score = node.find(attrs={"data-stat" : "opp_name"})
   print(opposing_score.text)
 
+def extract_opposing_team_name(node):
+  opposing_team_name = node.find(attrs={"data-stat" : "opp_name"})
+  print(opposing_team_name.text)
+
+
+def extract_opposing_team_score(node):
+  opposing_team_score = node.find(attrs={"data-stat" : "opp_pts"})
+  print(opposing_team_score.text)
+
+def extract_result(node):
+  game_result = node.find(attrs={"data-stat" : "game_result"}).text
+  if(game_result == 'W'):
+    print('home team won! :)')
+  else:
+    print('home team won lost :(')
+
 extract_opposing_team_name(rows[0])
+extract_opposing_team_score(rows[0])
+extract_result(rows[0])
